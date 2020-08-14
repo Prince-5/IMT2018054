90c9c51731d68bc375313a138a6364f28de14040
Pankaj <pankajkhe@gmail.com>
2020-08-15 01:23:36 +0530

    Added division function to assignment1.py

--- assignment1.py
+++ assignment1.py
@@ -4,12 +4,19 @@ def addition(a,b):
 def subtraction(a,b):
 	return a-b;
 
+def division(a,b):
+	if(b==0):
+		return "ZeroDivisionError"
+	else:
+		return (1.0*a)/b
+
 
 if __name__ == "__main__":
 	a = 3
 	b = 5
 	print(addition(a,b))
 	print(subtraction(a,b))
+	print(division(a,b))
 
 
 '''
